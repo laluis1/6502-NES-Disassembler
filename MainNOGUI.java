fbe82a7be4a4ca136382b7d4a59e005d047d851e
Luis Alvernaz <laluis1@users.noreply.github.com>
2018-12-30 11:47:21 -0500

    Update MainNOGUI.java

--- MainNOGUI.java
+++ MainNOGUI.java
@@ -13,7 +13,7 @@ public class MainNOGUI {
 			System.err.println("Must specify file (Usage: 6502Disassembler.jar file.nes)");
 			System.exit(-1);
 		}
-		// Instructions from 0x8000 to 0xFFFF are saved as a string
+		// Instructions are saved as a string
 		String instructions = "";
 
 		// Dictionary for opcodes -> instruction name
@@ -22,7 +22,7 @@ public class MainNOGUI {
 		// Load file
 		File f = new File(args[0]);
 		
-		// Loading bytes of the nes file
+		// Loading bytes of the .nes file
 		byte[] bytes = null;
 		if (f.length() < Integer.MAX_VALUE) {
 			bytes = new byte[(int) f.length()];
@@ -34,7 +34,22 @@ public class MainNOGUI {
 		} else {
 			System.err.println("File too big");
 		}
-
+		// checking iNES header
+		if(!(bytes[0] == 0x4e && bytes[1] == 0x45 && bytes[2] == 0x53 && bytes[3] == 0x1a)) {
+			System.err.println("Wrong file type, this only works with ines format");
+			System.exit(0);
+		}
+		
+		int PRGsize = Byte.toUnsignedInt(bytes[4])*0x4000;
+		int offset = 0x10;
+		
+		boolean trainer = false;
+		
+		if(Integer.toBinaryString(bytes[6]).charAt(6-(9-Integer.toBinaryString(bytes[6]).length())) == '1') {
+			trainer = true;
+			offset += 512;
+		}
+		
 		// Taking opcode to instruction from file.ssv
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("file.ssv"));
@@ -45,10 +60,10 @@ public class MainNOGUI {
 			} while (a != null);
 		} catch (Exception e) {}
 		
-		//read bytes from 0x8000 to 0xFFFF in nes file and map them + save to instruction string
-		for (int i = 0x8010; i < 0x10010;) {
+		//read bytes from nes file and map them + save to instruction string
+		for (int i = offset; i < PRGsize + offset;) {
 			String byt = (Integer.toHexString(Byte.toUnsignedInt(bytes[i]))).toUpperCase();
-			String command = "0x" + Integer.toHexString(i - 0x10) + ":";
+			String command = "0x" + Integer.toHexString(i -  offset) + ":";
 			if (byt.length() < 2) {
 				byt = "0" + byt;
 			}
@@ -56,7 +71,7 @@ public class MainNOGUI {
 			try {
 				inc = Integer.parseInt(dict.get(byt)[1]);
 			} catch (Exception e) {
-
+				System.err.println("<Error>");
 				System.err.println(Integer.toHexString(12));
 				System.err.println(byt);
 				System.err.println(command);
